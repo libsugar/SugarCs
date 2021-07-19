37098b6bf486936037a6a5b38c7383fb720dbc73
2A5F <u2a5f@outlook.com>
2021-07-19 20:30:34 +0800

    TODO

--- LibSugar/Fn.cs
+++ LibSugar/Fn.cs
@@ -1,8 +1,35 @@
+using System;
+
 namespace LibSugar
 {
     public static partial class Sugar
     {
         public static T Identity<T>(T v) => v;
         public static void Empty() { }
+        
+        public static void TODO(string? message = "TODO")
+        {
+            throw new NotImplementedException(message);
+        }
+        public static void TODO(string? message, Exception? inner)
+        {
+            throw new NotImplementedException(message, inner);
+        }
+        public static void TODO(Exception? inner)
+        {
+            throw new NotImplementedException("TODO", inner);
+        }
+        public static T TODO<T>(string? message = "TODO")
+        {
+            throw new NotImplementedException(message);
+        }
+        public static T TODO<T>(string? message, Exception? inner)
+        {
+            throw new NotImplementedException(message, inner);
+        }
+        public static T TODO<T>(Exception? inner)
+        {
+            throw new NotImplementedException("TODO", inner);
+        }
     }
 }
