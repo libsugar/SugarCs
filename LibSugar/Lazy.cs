2ce78f911ca6205ef0e773b0a46d55687adff832
2A5F <u2a5f@outlook.com>
2022-09-18 11:26:20 +0800

    lazy no thread safe

--- LibSugar/Lazy.cs
+++ LibSugar/Lazy.cs
@@ -16,7 +16,7 @@ public abstract class LazyBy<T>
     /// <summary>
     /// Make an assoc Lazy
     /// </summary>
-    public static LazyBy<T> Create<P>(Func<P> selector, Func<T> getter) => new Impl<P>(getter, selector);
+    public static LazyBy<T> Create<P>(Func<P> selector, Func<T> getter, bool threadSafe = true) => threadSafe ? new Impl<P>(getter, selector) : new ImplSync<P>(getter, selector);
 
     /// <summary>
     /// Check is the value created
@@ -71,6 +71,44 @@ public abstract class LazyBy<T>
             }
         }
     }
+
+    class ImplSync<P> : LazyBy<T>
+    {
+        readonly Func<T> Getter;
+        readonly Func<P> Selector;
+
+        public ImplSync(Func<T> getter, Func<P> selector)
+        {
+            Getter = getter;
+            Selector = selector;
+        }
+
+        T lastValue = default!;
+        P lastParam = default!;
+
+        int isValueCreated = 0;
+
+        public override bool IsValueCreated => isValueCreated == 1;
+
+        public override T Value
+        {
+            get
+            {
+                if (Interlocked.Exchange(ref isValueCreated, 1) == 1)
+                {
+                    var param = Selector();
+                    if (EqualityComparer<P>.Default.Equals(lastParam, param)) return lastValue;
+                    lastParam = param;
+                    return lastValue = Getter();
+                }
+                else
+                {
+                    lastParam = Selector();
+                    return lastValue = Getter();
+                }
+            }
+        }
+    }
 }
 
 /// <summary>
@@ -81,19 +119,19 @@ public static class LazyFunc
     /// <summary>
     /// Make a lazy function
     /// </summary>
-    public static Func<T> Create<T>(Func<T> fn) => new Fn<T>(fn).Function;
+    public static Func<T> Create<T>(Func<T> fn, bool threadSafe = true) => new Fn<T>(fn, threadSafe).Function;
     /// <summary>
     /// Make a lazy function
     /// </summary>
-    public static Func<P1, T> Create<P1, T>(Func<P1, T> fn) => new Fn<P1, T>(fn).Function;
+    public static Func<P1, T> Create<P1, T>(Func<P1, T> fn, bool threadSafe = true) => threadSafe ? new Fn<P1, T>(fn).Function : new FnSync<P1, T>(fn).Function;
     /// <summary>
     /// Make a lazy function
     /// </summary>
-    public static Func<P1, P2, T> Create<P1, P2, T>(Func<P1, P2, T> fn) => new Fn<P1, P2, T>(fn).Function;
+    public static Func<P1, P2, T> Create<P1, P2, T>(Func<P1, P2, T> fn, bool threadSafe = true) => threadSafe ? new Fn<P1, P2, T>(fn).Function : new FnSync<P1, P2, T>(fn).Function;
     /// <summary>
     /// Make a lazy function
     /// </summary>
-    public static Func<P1, P2, P3, T> Create<P1, P2, P3, T>(Func<P1, P2, P3, T> fn) => new Fn<P1, P2, P3, T>(fn).Function;
+    public static Func<P1, P2, P3, T> Create<P1, P2, P3, T>(Func<P1, P2, P3, T> fn, bool threadSafe = true) => threadSafe ? new Fn<P1, P2, P3, T>(fn).Function : new FnSync<P1, P2, P3, T>(fn).Function;
 
     abstract record FnBase<P, T>
     {
@@ -118,9 +156,31 @@ public static class LazyFunc
         }
     }
 
+    abstract record FnBaseSync<P, T>
+    {
+        protected T lastValue = default!;
+        protected P lastParam = default!;
+        protected int isValueCreated = 0;
+
+        public bool Get(P param)
+        {
+            if (Interlocked.Exchange(ref isValueCreated, 1) == 1)
+            {
+                if (EqualityComparer<P>.Default.Equals(lastParam, param)) return true;
+                lastParam = param;
+                return false;
+            }
+            else
+            {
+                lastParam = param;
+                return false;
+            }
+        }
+    }
+
     class Fn<T> : Lazy<T>
     {
-        public Fn(Func<T> valueFactory) : base(valueFactory, true) { }
+        public Fn(Func<T> valueFactory, bool threadSafe) : base(valueFactory, threadSafe) { }
 
         public T Function() => Value;
     }
@@ -129,14 +189,26 @@ public static class LazyFunc
     {
         public T Function(P1 p1) { lock (locker) return Get(p1) ? lastValue : lastValue = Getter(p1); }
     }
+    record FnSync<P1, T>(Func<P1, T> Getter) : FnBaseSync<P1, T>()
+    {
+        public T Function(P1 p1) => Get(p1) ? lastValue : lastValue = Getter(p1);
+    }
 
     record Fn<P1, P2, T>(Func<P1, P2, T> Getter) : FnBase<(P1, P2), T>()
     {
         public T Function(P1 p1, P2 p2) { lock (locker) return Get((p1, p2)) ? lastValue : lastValue = Getter(p1, p2); }
     }
+    record FnSync<P1, P2, T>(Func<P1, P2, T> Getter) : FnBaseSync<(P1, P2), T>()
+    {
+        public T Function(P1 p1, P2 p2) => Get((p1, p2)) ? lastValue : lastValue = Getter(p1, p2);
+    }
 
     record Fn<P1, P2, P3, T>(Func<P1, P2, P3, T> Getter) : FnBase<(P1, P2, P3), T>()
     {
         public T Function(P1 p1, P2 p2, P3 p3) { lock (locker) return Get((p1, p2, p3)) ? lastValue : lastValue = Getter(p1, p2, p3); }
+    }
+    record FnSync<P1, P2, P3, T>(Func<P1, P2, P3, T> Getter) : FnBaseSync<(P1, P2, P3), T>()
+    {
+        public T Function(P1 p1, P2 p2, P3 p3) => Get((p1, p2, p3)) ? lastValue : lastValue = Getter(p1, p2, p3);
     }
 }
