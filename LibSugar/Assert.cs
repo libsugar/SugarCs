34a0a5c61ca5165936adce6c2384d0f66edc0e4b
2A5F <u2a5f@outlook.com>
2022-09-19 15:23:48 +0800

    fix unity no half

--- LibSugar/Assert.cs
+++ LibSugar/Assert.cs
@@ -9,7 +9,7 @@ namespace LibSugar;
 
 public static partial class Sugar
 {
-	public static bool AssertTrue(this bool v) => v ? v : throw new AssertException($"Expect true but actually is {v}");
+    public static bool AssertTrue(this bool v) => v ? v : throw new AssertException($"Expect true but actually is {v}");
     public static bool AssertFalse(this bool v) => v ? v : throw new AssertException($"Expect false but actually is {v}");
 
     public static T AssertEq<T>(this T self, T other) => EqualityComparer<T>.Default.Equals(self, other) ? self : throw new AssertException($"Expect {other} but actually is {self}");
@@ -28,17 +28,23 @@ public static partial class Sugar
     public static int AssertZero(this int v) => v == 0 ? v : throw new AssertException($"Expect 0 but actually is {v}");
     public static long AssertZero(this long v) => v == 0 ? v : throw new AssertException($"Expect 0 but actually is {v}");
     public static nint AssertZero(this nint v) => v == 0 ? v : throw new AssertException($"Expect 0 but actually is {v}");
+#if !UNITY
     public static Half AssertZero(this Half v) => v == (Half)0 ? v : throw new AssertException($"Expect 0 but actually is {v}");
+#endif
     public static float AssertZero(this float v) => v == 0 ? v : throw new AssertException($"Expect 0 but actually is {v}");
     public static double AssertZero(this double v) => v == 0 ? v : throw new AssertException($"Expect 0 but actually is {v}");
     public static decimal AssertZero(this decimal v) => v == 0 ? v : throw new AssertException($"Expect 0 but actually is {v}");
     public static BigInteger AssertZero(this BigInteger v) => v == 0 ? v : throw new AssertException($"Expect 0 but actually is {v}");
 
+#if !UNITY
     public static Half AssertNotNaN(this Half v) => !Half.IsNaN(v) ? v : throw new AssertException($"Expect not NaN but actually is {v}");
+#endif
     public static float AssertNotNaN(this float v) => !float.IsNaN(v) ? v : throw new AssertException($"Expect not NaN but actually is {v}");
     public static double AssertNotNaN(this double v) => !double.IsNaN(v) ? v : throw new AssertException($"Expect not NaN but actually is {v}");
 
+#if !UNITY
     public static Half AssertNaN(this Half v) => Half.IsNaN(v) ? v : throw new AssertException($"Expect NaN but actually is {v}");
+#endif
     public static float AssertNaN(this float v) => float.IsNaN(v) ? v : throw new AssertException($"Expect NaN but actually is {v}");
     public static double AssertNaN(this double v) => double.IsNaN(v) ? v : throw new AssertException($"Expect NaN but actually is {v}");
 
@@ -59,8 +65,8 @@ public static partial class SugarStruct
 [Serializable]
 public class AssertException : Exception
 {
-	public AssertException() { }
-	public AssertException(string message) : base(message) { }
-	public AssertException(string message, Exception inner) : base(message, inner) { }
-	protected AssertException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    public AssertException() { }
+    public AssertException(string message) : base(message) { }
+    public AssertException(string message, Exception inner) : base(message, inner) { }
+    protected AssertException(SerializationInfo info, StreamingContext context) : base(info, context) { }
 }
